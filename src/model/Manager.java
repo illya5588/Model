f3ee398139b0a4bc89866588cb13b403318b1e55
illyashulman <carat98@icloud.com>
2020-04-06 22:48:52 +0100

    .

--- src/model/Manager.java
+++ src/model/Manager.java
@@ -1,7 +1,23 @@
 package model;
 
+import java.time.LocalDate;
+import java.util.Objects;
+
 public class Manager {
- String surname;
- String name;
+    String surname;
+    String name;
+    LocalDate DOB;
+
+
+    public Manager() {
+    }
+
+    public Manager(String surname, String name, LocalDate DOB) {
+        this.surname = surname;
+        this.name = name;
+        this.DOB = DOB;
+    }
+
+
 
 }
--- src/model/Teacher.java
+++ src/model/Teacher.java
@@ -1,13 +1,25 @@
 package model;
 
+import java.time.LocalDate;
+import java.util.Map;
+
 public class Teacher {
     String surname;
     String name;
-    
+    LocalDate DOB;
 
-    void setMarks() {
-
+    public Teacher() {
     }
 
+    public Teacher(String surname, String name, LocalDate DOB) {
+        this.surname = surname;
+        this.name = name;
+        this.DOB = DOB;
+    }
 
+    public void setMark(Student student, Subject subject, int mark, Map marks) {
+
+        student.markUpdate(subject, mark, marks);
+        student.countAvarageMark(marks);
+    }
 }
